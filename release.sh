d9f5fccfc1eb7c282fa55066eb9cea7f3f130458
Nathaniel Mote <nat@natmote.net>
2012-10-21 11:25:19 -0700

    add blank lines to the output of the release script

--- release.sh
+++ release.sh
@@ -8,9 +8,11 @@ cd webapps/fullhouse/fullhouse
 echo fetching changes from branch '$1'
 git pull origin $1
 
+echo
 echo installing new dependencies
 pip install -r requirements.txt
 
+echo
 echo copying static files
 cp -r ~/webapps/fullhouse/fullhouse/fullhouse/static/* ~/webapps/fullhouse_static/
 "
