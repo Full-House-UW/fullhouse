d42efa9f8fbd12542ad5ca7521d5c55e99b74590
Nathaniel Mote <nat@natmote.net>
2012-10-21 11:19:02 -0700

    add release script

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ssh heff@heff.webfactional.com "
+source webapps/fullhouse/env/bin/activate
+
+cd webapps/fullhouse/fullhouse
+
+echo fetching changes from branch '$1'
+git pull origin $1
+
+echo installing new dependencies
+pip install -r requirements.txt
+
+echo copying static files
+cp -r ~/webapps/fullhouse/fullhouse/fullhouse/static/* ~/webapps/fullhouse_static/
+"
