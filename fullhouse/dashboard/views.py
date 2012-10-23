005af0719ce62c13d0e6bc4fb491d49cc2870e1d
Jeff Lowdermlk <tiro.jeff.ml@gmail.com>
2012-10-23 15:08:28 -0700

    Add pep8 test, fix pep8 errors
    Running './manage.py test' will now also check all source files
    for pep8 compliance. Add pep8 to requirements.txt

--- fullhouse/dashboard/views.py
+++ fullhouse/dashboard/views.py
@@ -2,18 +2,23 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 
+
 def home(request):
     return HttpResponseRedirect('/welcome/')
+
 
 def welcome(request):
     return render_to_response('welcome.html',
         RequestContext(request, {}))
 
+
 def login(request):
     return HttpResponseRedirect('/dashboard/')
 
+
 def logout(request):
     return HttpResponseRedirect('/')
+
 
 def dashboard(request):
     return render_to_response('dashboard.html')
