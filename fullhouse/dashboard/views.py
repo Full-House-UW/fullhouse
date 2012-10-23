d950cadc21f9b31411a526f144eb66df16f8c62f
Conor Reisman <creisman@cs.washington.edu>
2012-10-22 20:47:10 -0700

    Changed templates to use inheritance and made a unified master CSS file.

--- fullhouse/dashboard/views.py
+++ fullhouse/dashboard/views.py
@@ -22,6 +22,7 @@ def logout(request):
 
 def dashboard(request):
     return render_to_response('dashboard.html')
+
     
 def registration(request):
     return render_to_response('registration.html')
