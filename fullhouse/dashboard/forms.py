7dba9cb6d3b7301d169a8f70a82f7f904ad05579
Jeff Lowdermilk <tiro.jeff.ml@gmail.com>
2012-12-06 16:10:40 -0800

    Add bootbox for dialogs, make leaving house a separate button
    bootbox gives easy js functions for making dialogs (builds on top
    of twitter bootstrap).
    leaving house is now a button with a confirm dialog that has a
    custom message (stronger warning if user is last house member).

--- fullhouse/dashboard/forms.py
+++ fullhouse/dashboard/forms.py
@@ -11,8 +11,6 @@ import models
 
 
 class CreateHouseForm(forms.ModelForm):
-    remove_from_house = forms.BooleanField(required=False, label="Remove me from this house")
-
     class Meta:
         model = models.House
 
